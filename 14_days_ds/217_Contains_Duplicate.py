5d2ea9ae28cbd50c62ef2dab375ae28c5f02d07f
shadhin <shahinur.rahman@evaly.com.bd>
2021-10-13 04:07:54 +0600

    optimize

--- 14_days_ds/217_Contains_Duplicate.py
+++ 14_days_ds/217_Contains_Duplicate.py
@@ -4,8 +4,4 @@ from collections import Counter
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         num = Counter(nums).values()
-        for value in num:
-            if value >= 2:
-                return True
-            else:
-                return False
+        return [value for value in num if value >= 2]
