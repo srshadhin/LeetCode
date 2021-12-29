819b9ee4d21d84052690275501b50b655819b6d0
Shahinur Rahman <shadhin.int@gmail.com>
2021-12-30 00:03:36 +0600

    test

--- 14_days_ds/217_Contains_Duplicate.py
+++ 14_days_ds/217_Contains_Duplicate.py
@@ -6,6 +6,12 @@ from collections import Counter
 
 
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(nums) -> bool:
         num = Counter(nums).values()
         return [value for value in num if value >= 2]
+
+
+if __name__ == '__main__':
+    nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+    obj_of_sol = Solution.containsDuplicate(nums=nums)
+    print(obj_of_sol)
