b841c09570cb642a29a2e7cbe2df064cd312e626
Shahinur Rahman <shadhin.int@gmail.com>
2022-01-09 22:59:20 +0600

    optimised

--- 14_days_ds/217_Contains_Duplicate.py
+++ 14_days_ds/217_Contains_Duplicate.py
@@ -12,6 +12,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-    nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+    nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2,4]
     obj_of_sol = Solution.containsDuplicate(nums=nums)
     # print(obj_of_sol)
