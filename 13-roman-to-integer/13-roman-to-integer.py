4822a21f15c5ff1e3eb576bb15412c5e65e1631d
Shahinur R. Shadhin <shadhin.int@gmail.com>
2022-01-14 22:23:47 +0600

    Time: 92 ms (7.71%), Space: 14.3 MB (28.23%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romman_dict = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+        
+        n = len(s)
+        rt = 0
+        for i in range(n):
+            if  i==n-1 or romman_dict[s[i]] >= romman_dict[s[i+1]] :
+                rt += romman_dict[s[i]]
+            else :
+                rt -= romman_dict[s[i]]
+                
+        return rt
+        
