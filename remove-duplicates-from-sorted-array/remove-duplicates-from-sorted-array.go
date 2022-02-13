7a236efc252e952bc069db0512890cb5a496dab7
Shahinur R. Shadhin <shadhin.int@gmail.com>
2022-02-13 23:43:25 +0600

    Time: 19 ms, Memory: 4.4 MB - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+    ln := len(nums)
+    if ln <= 1 {
+        return ln
+    }
+    
+    j := 0 // points to  the index of last filled position
+    for i := 1; i < ln; i++ {
+        if nums[j] != nums[i] {
+            j++
+            nums[j] = nums[i]
+        }
+    }
+    
+    return j + 1
+}
