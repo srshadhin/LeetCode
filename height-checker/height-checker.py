d9578f628730699bdf788f121dec1885dac5a6fc
Shahinur R. Shadhin <shadhin.int@gmail.com>
2022-02-27 21:54:49 +0600

    Time: 32 ms, Memory: 13.8 MB - LeetHub

--- height-checker/height-checker.py
+++ height-checker/height-checker.py
@@ -2,7 +2,12 @@ class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         sorted_list = sorted(heights)
         counter = 0
-        for i in range(len(heights)):
-            if heights[i] != sorted_list[i]:
-                counter+=1
+        # Solution 1
+        # for i in range(len(heights)):
+        #     if heights[i] != sorted_list[i]:
+        #         counter+=1
+        # return counter
+        for x, y in zip(heights, sorted_list):
+            if x != y:
+                counter +=1
         return counter
