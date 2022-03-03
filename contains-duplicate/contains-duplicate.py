1e52b4a7c423e2cfaab236b5106650977132be61
Shahinur R. Shadhin <shadhin.int@gmail.com>
2022-03-03 23:48:37 +0600

    Time: 806 ms, Memory: 25.6 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        num = Counter(nums).values()
+        return [value for value in num if value>=2]
+        
+        
+        
