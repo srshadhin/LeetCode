8a1f25ba23683bf774a0d451dbb820b2db3dfb7a
Shahinur R. Shadhin <shadhin.int@gmail.com>
2022-03-04 15:50:59 +0600

    Time: 204 ms, Memory: 18.4 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -9,14 +9,14 @@ class Solution:
         #     s.insert(i, s.pop())
         #     i+=1
         
-        #2nd Solution
-        for i in range(len(s)//2):
-            s[i], s[-i-1] = s[-i-1], s[i]
+        # #2nd Solution
+        # for i in range(len(s)//2):
+        #     s[i], s[-i-1] = s[-i-1], s[i]
         
         #3rd Solution
-        # f, l = 0, len(s)-1
-        # while f <l:
-        #     s[f],  s[l] = s[l], s[f]
-        #     f, l = f+1, l-1
+        f, l = 0, len(s)-1
+        while f <l:
+            s[f],  s[l] = s[l], s[f]
+            f, l = f+1, l-1
         
         
