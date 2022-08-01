223e15459d349da8fb999a6be49b0ef93b70a005
Shahinur R. Shadhin <shadhin.int@gmail.com>
2022-08-01 18:41:33 +0600

    Time: 163 ms (89.26%), Space: 15.3 MB (48.86%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -4,7 +4,7 @@ class Solution:
         left_sum = 0
         # right_sum = 0
         for num in range(len(nums)):
-            right_sum = total_sum - left_sum-nums[num]     
+            right_sum = total_sum - (left_sum+nums[num])     
             if left_sum == right_sum:
                 return num
             left_sum +=nums[num]
