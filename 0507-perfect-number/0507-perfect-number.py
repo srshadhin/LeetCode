636422e6a044687b10afe3c96030e175af005484
Shahinur R. Shadhin <shadhin.int@gmail.com>
2024-02-15 10:36:42 +0600

    Time: 46 ms (31.12%), Space: 16.6 MB (83.28%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+        
+        sum_of = 1  # 1 is always a proper divisor
+        sqrt_num = int(num ** 0.5)
+        for i in range(2, sqrt_num + 1):
+            if num % i == 0:
+                sum_of += i
+                if i != num // i:  # Exclude perfect squares
+                    sum_of += num // i
+        
+        return sum_of == num
